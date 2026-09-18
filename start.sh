3310594e7a1ff49198b62c91de78d4dd561b60d0
gumballminecraft95-cmd <gumballminecraft95@gmail.com>
2026-09-18 16:46:04 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,14 @@
 export HOME=/root
 export USER=root
 
+rm -rf ~/.vnc ~/.config/tigervnc
 mkdir -p ~/.vnc
+
 echo "password" | vncpasswd -f > ~/.vnc/passwd
 chmod 600 ~/.vnc/passwd
+
+echo "startxfce4 &" > ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
 
 rm -rf /tmp/.X1-lock /tmp/.X11-unix/X1
 
@@ -12,4 +17,4 @@ vncserver :1 -geometry 1280x720 -depth 24
 sleep 3
 
 PORT="${PORT:-8080}"
-websockify --web /usr/share/novnc/ --wrap-mode=ignore $PORT localhost:5901
+websockify --web /usr/share/novnc/ $PORT localhost:5901
