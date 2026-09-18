fbdc71ffbb248db6c034f62e11efe55b50b372b8
gumballminecraft95-cmd <gumballminecraft95@gmail.com>
2026-09-18 16:39:52 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export HOME=/root
+export USER=root
 
 mkdir -p ~/.vnc
 echo "password" | vncpasswd -f > ~/.vnc/passwd
@@ -7,6 +9,7 @@ chmod 600 ~/.vnc/passwd
 rm -rf /tmp/.X1-lock /tmp/.X11-unix/X1
 
 vncserver :1 -geometry 1280x720 -depth 24
+sleep 3
 
 PORT="${PORT:-8080}"
 websockify --web /usr/share/novnc/ --wrap-mode=ignore $PORT localhost:5901
